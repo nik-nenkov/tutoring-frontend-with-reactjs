63a68d09bc3389379907814cdad502c7fb604805
Nikolay Nenkov <n.nenkov@yahoo.com>
2018-02-17 08:51:57 +0200

    test commit from VS Code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import './App.css';
 import BookRow from './BookRow';
 const API = "http://localhost:8080";
-//const API = "http://192.168.43.166:8080";
+//const API = "http://192.168.43.166:8080";  v0.2.1
 const Title = styled.h1`
 	font-size:2.1em;
 	text-align: center;
